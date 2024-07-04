9e1ed63a0144135f1c0978cd3d57192727265a50
Zikiro <Zikiro@dot-tech.net>
2024-07-04 12:39:54 -0400

    Loop With Self Clean Up
    - Looping 3x3 area array with self clean up at the start.

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+clear()
+while get_pos_x() != 0:
+    move(North)
+        
+while get_pos_y() != 0:
+    move(East)
+
 while True:
     if get_entity_type() == Entities.Grass:
         plant(Entities.Bush)
+        move(North)
     elif can_harvest():
         harvest()
+        move(North)
     else:
-        if get_pos_y() != 0:
+        if get_pos_y() == 0:
+            move(East)
             plant(Entities.Bush)
-            move(North)
-        elif can_harvest():
-            harvest()
+        else:
             move(North)
