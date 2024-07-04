945c8d24517e6b8ea0b8e1ded4c60498f7e0baa0
Zikiro <Zikiro@dot-tech.net>
2024-07-04 14:30:46 -0400

    More Efficient Wood
    - Updated the script to get wood even more efficiently.

--- wood-getter.py
+++ wood-getter.py
@@ -1,31 +1,39 @@
-clear()
+# Script settings
+shouldClear = False
 
-x = get_pos_x()
-y = get_pos_y()
 
-while x != 0:
+if shouldClear: 
+    clear()
+
+while get_pos_y() != 0:
     move(North)
         
-while y != 0:
+while get_pos_x() != 0:
     move(East)
     
     
-def treeOrHay():
+def treeOrBush():
+    x = get_pos_x()
+    y = get_pos_y()
+    oddCords = (x % 2) == 1 and (y % 2) == 1
+    evenCords = (x % 2) == 0 and (y % 2) == 0
+    isTreePos = oddCords or evenCords
+
     if can_harvest():
         harvest()
     else:
         return
-    if get_pos_x() != 1 and get_pos_y() != 1:
+    if isTreePos:
         plant(Entities.Tree)
     else:
         plant(Entities.Bush)
 
 while True:
     if get_pos_y() != 0:
-        treeOrHay()
+        treeOrBush()
         move(North)
     else: 
         move(East)
-        treeOrHay()
+        treeOrBush()
         move(North)
     
